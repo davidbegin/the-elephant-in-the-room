509246386e8fa52424cdd37eedf5f8a1ec2724b7
David Begin <davidmichaelbe@gmail.com>
2015-05-10 19:37:40 -0700

    oooweee, did I just connect to a db in Rust!?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+extern crate postgres;
 mod the_printers;
+
+use postgres::{Connection, SslMode};
 
 fn main() {
     the_printers::title();
+
+    let dsn = "postgresql://dbegin@localhost/first_rust_db";
+
+    let conn = match Connection::connect(dsn, &SslMode::None) {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("An Error: {:?}", e);
+            return;
+        }
+    };
+
+
+    println!("{:?}", conn);
 }
